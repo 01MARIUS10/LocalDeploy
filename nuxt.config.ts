cbb3c363a792b9bae5a56991c965cfec54412c91
Narindra1 <sahobynomenjanahary2@gmail.com>
2025-12-17 21:16:33 +0300

    dynamisation des données de project slug

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,21 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss'],
-  
+  modules: ["@nuxtjs/tailwindcss"],
+
   // Configuration pour utiliser le dossier app/
-  srcDir: 'app/',
-  
+  srcDir: "app/",
+
   // Désactiver le service worker warning
   nitro: {
     routeRules: {
-      '/service-worker.js': { 
-        headers: { 
-          'cache-control': 'public, max-age=0, must-revalidate' 
-        } 
+      "/service-worker.js": {
+        headers: {
+          "cache-control": "public, max-age=0, must-revalidate",
+        },
       },
-      '/profil': { ssr: false } // Désactiver le SSR pour la page profil
-    }
-  }
-})
+      "/profil": { ssr: false }, // Désactiver le SSR pour la page profil
+      "/projects/[slug]": { ssr: false }, // rendu client
+    },
+  },
+});
